fe534c564416cb52730d1344e21f53eb0e37eef3
YaroslavNurtdinov <nurtdinovyaroslav@gmail.com>
2021-12-10 19:40:11 +0200

    done: ViewPager, viewPager adapter, Parcelize, Overview xml, Overview fragment.

--- app/src/main/java/com/example/nurtdinov/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/example/nurtdinov/adapters/RecipesAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.nurtdinov.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
@@ -13,7 +15,9 @@ import com.example.nurtdinov.R
 import com.example.nurtdinov.databinding.RecipesRowLayoutBinding
 import com.example.nurtdinov.models.FoodRecipe
 import com.example.nurtdinov.models.Result
+import com.example.nurtdinov.ui.fragments.recipes.RecipesFragmentDirections
 import com.example.nurtdinov.util.RecipesDiffUtil
+import org.jsoup.Jsoup
 
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
     private var recipes = emptyList<Result>()
@@ -21,20 +25,41 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = RecipesRowLayoutBinding.bind(itemView)
 
-        fun bind(result: Result) = with(binding) {
-
-            recipeImageView.load(result.image){
-                crossfade(300)
+        fun parseHtml(textView: TextView, description:String?){
+            if(description != null){
+                val desc = Jsoup.parse(description).text()
+                textView.text = desc
             }
-            titleTextView.text = result.title
-            descriptionTextView.text = result.summary
-            heartTextView.text = result.aggregateLikes.toString()
-            clockTextView.text = result.readyInMinutes.toString()
-
 
         }
 
+        fun bind(result: Result) = with(binding) {
+            recipeImageView.load(result.image) {
+                crossfade(300)
+            }
+            titleTextView.text = result.title
+           // descriptionTextView.text = result.summary
+            parseHtml(descriptionTextView,result.summary)
+            heartTextView.text = result.aggregateLikes.toString()
+            clockTextView.text = result.readyInMinutes.toString()
+
+            binding.root.setOnClickListener {
+                Log.d("Called","called")
+                try {
+                    val action =
+                        RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    binding.root.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("RowClick", e.toString())
+                }
+
+            }
+        }
+
     }
+
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(
@@ -46,11 +71,11 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
         val currentRecipe = recipes[position]
         holder.bind(currentRecipe)
 
-       if (currentRecipe.vegan){
+        if (currentRecipe.vegan) {
             holder.itemView.findViewById<TextView>(R.id.leaf_textView)
-                .setTextColor(ContextCompat.getColor(holder.itemView.context,R.color.green))
+                .setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
             holder.itemView.findViewById<ImageView>(R.id.leaf_imageView)
-                .setColorFilter(ContextCompat.getColor(holder.itemView.context,R.color.green))
+                .setColorFilter(ContextCompat.getColor(holder.itemView.context, R.color.green))
         }
     }
 
---
+++ app/src/main/java/com/example/nurtdinov/ui/fragments/overview/OverviewFragment.kt
@@ -0,0 +1,78 @@
+package com.example.nurtdinov.ui.fragments.overview
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import coil.load
+import com.example.nurtdinov.R
+import com.example.nurtdinov.databinding.FragmentOverviewBinding
+import com.example.nurtdinov.models.Result
+import org.jsoup.Jsoup
+
+class OverviewFragment : Fragment() {
+
+    private var _binding: FragmentOverviewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOverviewBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        binding.apply {
+            mainImageView.load(myBundle?.image)
+            titleTextView.text = myBundle?.title
+            likesTextView.text = myBundle?.aggregateLikes.toString()
+            timeTextView.text = myBundle?.readyInMinutes.toString()
+            myBundle?.summary.let {
+                val summary = Jsoup.parse(it).text()
+                summaryTextView.text = summary
+            }
+        }
+
+        if (myBundle?.vegetarian == true) {
+            binding.vegetarianImageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            binding.vegetarianTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.green))
+        }
+
+        if (myBundle?.vegan == true) {
+            binding.veganImageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            binding.veganTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.green))
+        }
+
+        if (myBundle?.glutenFree == true) {
+            binding.glutenFreeImageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            binding.glutenFreeTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.green))
+        }
+
+        if (myBundle?.dairyFree == true) {
+            binding.dairyFreeImageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            binding.dairyFreeTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.green))
+        }
+
+        if (myBundle?.veryHealthy == true) {
+            binding.healthyImageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            binding.healthyTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.green))
+        }
+
+        if (myBundle?.cheap == true) {
+            binding.cheapImageView.setColorFilter(ContextCompat.getColor(requireContext(),R.color.green))
+            binding.cheapTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.green))
+        }
+
+
+    }
+}
